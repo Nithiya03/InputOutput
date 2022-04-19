e46f5cff100c0998ebde2d65e39d23be0ec7ca8d
Nithiya03 <nithiyasrirud03@gmail.com>
2022-04-19 15:32:00 +0530

    angular

---
+++ src/app/other/simple.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input,OnChanges, SimpleChanges} from "@angular/core";
+
+@Component({
+    selector:'simple',
+    template:'you entered : {{simpleInput}}'
+})
+export class simplecomponent implements OnChanges{
+    @Input() simpleInput:string='';
+
+    ngOnChanges(changes: SimpleChanges): void {
+        for(let propertyName in changes){
+            let change = changes[propertyName];
+            let current = JSON.stringify(change.currentValue);
+            let previous = JSON.stringify(change.currentValue);
+            console.log(propertyName + ':currentValue - ' + current +'previousValue - '+previous);
+        }
+    }
+}
